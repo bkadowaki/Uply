057abc92a86ba30894e46cc348bb83444d5f299d
Ian Florentino <ianflorentino88@gmail.com>
2014-10-27 14:44:51 -0700

    added gems for screenshot and dropped screenshot column on websites table

--- db/migrate/20141027211045_add_favicon_to_websites.rb
+++ db/migrate/20141027211045_add_favicon_to_websites.rb
@@ -1,6 +1,5 @@
 class AddFaviconToWebsites < ActiveRecord::Migration
   def change
     add_column :websites, :favicon, :string
-    add_column :websites, :screenshot, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027185417) do
+ActiveRecord::Schema.define(version: 20141027211045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,6 @@ ActiveRecord::Schema.define(version: 20141027185417) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "favicon"
-    t.string   "screenshot"
   end
 
 end
